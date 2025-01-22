60f7a83e8af9f114f9ddfb1bd3dd38645f3ab4d9
oakinwuntan <oakinwuntan@localhost>
2025-01-22 02:41:32 -0600

    I used the code to draw a tree

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,49 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(20);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(220);
+		robot.turnLeft(90);
+		robot.forward(60);
 		robot.turnRight(90);
 		robot.forward(80);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(80);
+		robot.turnLeft(90);
+		robot.forward(60);
+		robot.turnRight(90);
+		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(80);
+		robot.turnRight(90);
+		robot.forward(60);
+		robot.turnLeft(90);
+		robot.forward(220);
+		robot.turnLeft(90);
+		robot.forward(20);
+	}
 	}
 
-}
+
